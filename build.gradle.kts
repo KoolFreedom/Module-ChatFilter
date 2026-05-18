0549f86e5190de7e395d5663f2c429b4eda77995
gamingto12 <33136250+gamingto12@users.noreply.github.com>
2026-05-18 16:29:18 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+repositories {
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+
+    maven {
+        url = uri("https://nexus.telesphoreo.me/repository/plex/")
+    }
+
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+    compileOnly("io.papermc.paper:paper-api:1.21.7-R0.1-SNAPSHOT")
+    compileOnly("dev.plex:server:1.5")
+}
+
+group = "dev.plex"
+version = "1.0"
+description = "ExampleModule"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+tasks.getByName<Jar>("jar") {
+    archiveBaseName.set("Module-ExampleModule")
+    archiveVersion.set("")
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+}
